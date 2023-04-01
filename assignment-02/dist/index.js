614faf78312f66651cb6979660419a2d1de04d57
Saddam <sadamumer566@gmail.com>
2023-04-01 23:09:34 +0500

    TS Assignment 2

--- assignment-02/dist/index.js
+++ assignment-02/dist/index.js
@@ -19,62 +19,129 @@ class Vehicle {
         return this._rented;
     }
     // ---------
-    set make(m) {
-        this._make = m;
+    set make(newMake) {
+        this._make = newMake;
     }
-    set model(m) {
-        this._model = m;
+    set model(newModel) {
+        this._model = newModel;
     }
-    set year(m) {
-        this._year = m;
+    set year(newYear) {
+        this._year = newYear;
     }
-    set rented(m) {
-        this._rented = m;
+    set rented(newRented) {
+        this._rented = newRented;
     }
     rent() {
         if (this.rented) {
-            console.log("This vehicle is already rented.");
+            console.log("The vehicle (" + this._make + " " + this._model + ") is already rented.");
         }
         else {
             this.rented = true;
-            console.log("This vehicle is now rented.");
+            console.log("The vehicle (" + this._make + " " + this._model + ") is now rented.");
         }
     }
     return() {
         if (this.rented) {
             this.rented = false;
-            console.log("This vehicle has now been returned.");
+            console.log("The vehicle (" + this._make + " " + this._model + ") has now been returned.");
         }
         else {
-            console.log("This vehicle has already been returned.");
+            console.log("The vehicle (" + this._make + " " + this._model + ") has already been returned.");
         }
     }
 }
 class Car extends Vehicle {
+    constructor(make, model, year, rented, fuelType, numOfSeats) {
+        super(make, model, year, rented);
+        this._fuelType = fuelType;
+        this._numOfSeats = numOfSeats;
+    }
+    get fuelType() {
+        return this._fuelType;
+    }
+    get numOfSeats() {
+        return this._numOfSeats;
+    }
+    // ---------
+    set fuelType(newFuelType) {
+        this.fuelType = newFuelType;
+    }
+    set numOfSeats(newNumOfSeats) {
+        this._numOfSeats = newNumOfSeats;
+    }
     rentalRate() {
-        return 60;
+        return 50;
     }
 }
 class Truck extends Vehicle {
+    constructor(make, model, year, rented, loadCapacity, numOfTyres) {
+        super(make, model, year, rented);
+        this._loadCapacity = loadCapacity;
+        this._numOfTyres = numOfTyres;
+    }
+    get loadCapacity() {
+        return this._loadCapacity;
+    }
+    get numOfTyres() {
+        return this._numOfTyres;
+    }
+    // ---------
+    set loadCapacity(newLoadCapacity) {
+        this._loadCapacity = newLoadCapacity;
+    }
+    set numOfTyres(newNumOfTyres) {
+        this._numOfTyres = newNumOfTyres;
+    }
     rentalRate() {
-        return 80;
+        return 90;
     }
 }
 class Bike extends Vehicle {
-    constructor(make, model, year, rented, qual) {
+    constructor(make, model, year, rented, tankCapacity, rideStyle, breakType) {
         super(make, model, year, rented);
-        this._qual = qual;
+        this._tankCapacity = tankCapacity;
+        this._rideStyle = rideStyle;
+        this._breakType = breakType;
     }
-    get() {
-        console.log(this.make);
+    get rideStyle() {
+        return this._rideStyle;
+    }
+    get breakType() {
+        return this._breakType;
+    }
+    get tankCapacity() {
+        return this._tankCapacity;
+    }
+    // ---------
+    set rideStyle(newRideStyle) {
+        this.rideStyle = newRideStyle;
+    }
+    set breakType(newBreakType) {
+        this.breakType = newBreakType;
+    }
+    set tankCapacity(newTankCapacity) {
+        this._tankCapacity = newTankCapacity;
     }
     rentalRate() {
-        return 30;
+        return 20;
     }
 }
-let b1 = new Bike(13, 45, 67, false, "good");
-b1.rent();
-b1.return();
-b1.return();
-console.log(b1.rented);
+let car1 = new Car("Honda", "Civic", 2016, false, "Diesel", 4);
+console.log(car1.model);
+console.log("type of fuel: " + car1.fuelType);
+car1.rent();
+car1.return();
+console.log("");
+let truck1 = new Truck("Foton", "Master M-280", 2021, false, 3, 8);
+console.log(truck1.model);
+truck1.return();
+truck1.rent();
+console.log("rental rate for truck: " + truck1.rentalRate());
+console.log("");
+let bike1 = new Bike("Unique", "CD-125", 2017, false, 18, "Upright", "Drum");
+console.log(bike1.model);
+bike1.rent();
+bike1.return();
+bike1.return();
+console.log("is bike rented: " + bike1.rented);
 //# sourceMappingURL=index.js.map
--- assignment-02/src/index.ts
+++ assignment-02/src/index.ts
@@ -1,11 +1,11 @@
 abstract class Vehicle
 {
-    private _make:number;
-    private _model:number;
+    private _make:string;
+    private _model:string;
     private _year:number;
     private _rented:boolean;
     
-    constructor(make:number,model:number,year:number,rented:boolean)
+    constructor(make:string,model:string,year:number,rented:boolean)
     {
         this._make=make;
         this._model=model;
@@ -31,21 +31,21 @@ abstract class Vehicle
     }
 
     // ---------
-    set make(m:number)
+    set make(newMake:string)
     {
-        this._make = m;
+        this._make = newMake;
     }
-    set model(m:number)
+    set model(newModel:string)
     {
-        this._model = m;
+        this._model = newModel;
     }
-    set year(m:number)
+    set year(newYear:number)
     {
-        this._year = m;
+        this._year = newYear;
     }
-    set rented(m:boolean)
+    set rented(newRented:boolean)
     {
-        this._rented = m;
+        this._rented = newRented;
     }
 
 
@@ -55,71 +55,172 @@ abstract class Vehicle
     {
         if(this.rented)
         {
-            console.log("This vehicle is already rented.");
+            console.log("The vehicle ("+this._make+" "+this._model+") is already rented.");
         }
         else
         {
             this.rented= true;
-            console.log("This vehicle is now rented.");
+            console.log("The vehicle ("+this._make+" "+this._model+") is now rented.");
         }
     }
     return():void
     {
         if(this.rented)
         {
-            this.rented= false;
-            console.log("This vehicle has now been returned.");
+            this.rented = false;
+            console.log("The vehicle ("+this._make+" "+this._model+") has now been returned.");
         }
         else
         {
-            console.log("This vehicle has already been returned.");
+            console.log("The vehicle ("+this._make+" "+this._model+") has already been returned.");
         }
     }
 
 }
 
+type FuelType= "Gasoline" | "Diesel" | "Electric";
 class Car extends Vehicle
 {
+    private _fuelType:FuelType;
+    private _numOfSeats:number;
+
+    constructor(make:string,model:string,year:number,rented:boolean,fuelType:FuelType,numOfSeats:number)
+    {
+       super(make,model,year,rented);
+       this._fuelType = fuelType;
+       this._numOfSeats = numOfSeats;
+    }
+
+    get fuelType()
+    {
+        return this._fuelType;
+    }
+    get numOfSeats()
+    {
+        return this._numOfSeats;
+    }
+
+    // ---------
+    set fuelType(newFuelType:FuelType)
+    {
+        this.fuelType = newFuelType;
+    }
+    set numOfSeats(newNumOfSeats:number)
+    {
+        this._numOfSeats = newNumOfSeats;
+    }
+
 
     rentalRate(): number 
     {
-        return 60;
+        return 50;
     }
 }
 
 class Truck extends Vehicle
 {
+    private _loadCapacity:number;    
+    private _numOfTyres:number;
+
+    constructor(make:string,model:string,year:number,rented:boolean,loadCapacity:number,numOfTyres:number)
+    {
+       super(make,model,year,rented);
+       this._loadCapacity = loadCapacity;
+       this._numOfTyres = numOfTyres;
+    }
+
+    get loadCapacity()
+    {
+        return this._loadCapacity;
+    }
+    get numOfTyres()
+    {
+        return this._numOfTyres;
+    }
+    // ---------
+    set loadCapacity(newLoadCapacity:number)
+    {
+        this._loadCapacity = newLoadCapacity;
+    }
+    set numOfTyres(newNumOfTyres:number)
+    {
+        this._numOfTyres = newNumOfTyres;
+    }
+
 
     rentalRate(): number 
     {
-        return 80;
+        return 90;
     }
 }
 
+type RideStyle= "Upright" | "Sport";
+type BreakType= "Drum" | "Disc";
 class Bike extends Vehicle
 {
-    private _qual:string;
+    private _tankCapacity:number;
+    private _rideStyle:RideStyle;
+    private _breakType:BreakType;
 
-    constructor(make:number,model:number,year:number,rented:boolean,qual:string)
+    constructor(make:string,model:string,year:number,rented:boolean,tankCapacity:number,rideStyle:RideStyle,breakType:BreakType)
     {
        super(make,model,year,rented);
-       this._qual = qual;
+       this._tankCapacity = tankCapacity;
+       this._rideStyle = rideStyle;
+       this._breakType = breakType;
     }
 
-    get()
+    get rideStyle()
     {
-        console.log(this.make);
+        return this._rideStyle;
+    }
+    get breakType()
+    {
+        return this._breakType;
+    }
+    get tankCapacity()
+    {
+        return this._tankCapacity;
     }
 
+    // ---------
+    set rideStyle(newRideStyle:RideStyle)
+    {
+        this.rideStyle = newRideStyle;
+    }
+    set breakType(newBreakType:BreakType)
+    {
+        this.breakType = newBreakType;
+    }
+    set tankCapacity(newTankCapacity:number)
+    {
+        this._tankCapacity = newTankCapacity;
+    }
+
+    
     rentalRate(): number {
-        return 30;
+        return 20;
     }
 }
 
-let b1 = new Bike(13,45,67,false,"good");
+let car1 = new Car("Honda","Civic",2016,false,"Diesel",4);
+console.log(car1.model);
+console.log("type of fuel: "+car1.fuelType);
+car1.rent();
+car1.return();
+console.log("");
 
-b1.rent();
-b1.return();
-b1.return();
+let truck1 = new Truck("Foton","Master M-280",2021,false,3,8);
+console.log(truck1.model);
+truck1.return();
+truck1.rent();
+console.log("rental rate for truck: "+truck1.rentalRate());
 
-console.log(b1.rented);
+console.log("");
+
+let bike1 = new Bike("Unique","CD-125",2017,false,18,"Upright","Drum");
+console.log(bike1.model);
+bike1.rent();
+bike1.return();
+bike1.return();
+console.log("is bike rented: "+bike1.rented);
